9a629df6d413563dd673bb9a4219ae59f7ac66c9
ICEFFX1 <davidfresh99@yahoo.com>
2025-06-27 15:50:47 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ const settings = {
   author: '‎',
   botName: "ROSE",
   botOwner: 'ICE', // Your name
-  ownerNumber: '2348122775305', //Set your number here without + symbol, just add country code & number without any space
+  ownerNumber: '2349065006974', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
