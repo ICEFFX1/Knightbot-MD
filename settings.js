79feeb766ebd6e67f48cd991dea6aa338c9dd1a2
ICEFFX1 <davidfresh99@yahoo.com>
2025-06-29 06:28:58 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'Knight Bot',
+  packname: 'ICE BOT',
   author: '‎',
-  botName: "ROSE",
+  botName: "SHADOW",
   botOwner: 'ICE', // Your name
-  ownerNumber: '2349065006974', //Set your number here without + symbol, just add country code & number without any space
+  ownerNumber: '2348122775305', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
